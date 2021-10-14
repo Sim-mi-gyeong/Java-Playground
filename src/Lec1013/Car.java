8919a96a9ece02006c774e0ea5abe5ceee41f241
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2021-10-14 23:30:39 +0900

    1013 Conv2

---
+++ src/Lec1013/Car.java
@@ -0,0 +1,19 @@
+package Lec1013;
+
+public class Car {
+    private int highest;
+    Navigator Navi;   // Navigator가 구성관계로 -> 두 개의 클래스에서 다른 하나의 클래스를 현재 클래스의 Field로 만들 때 레퍼런스로 선언 =>
+
+    Car() {
+        highest = 140 + 10*(int)(Math.random()*8);   // 속도 구간 140~210
+        Navi = new Navigator();
+    }
+    // 최고속도
+    int getHighest() {
+        return highest;
+    }
+    int current() {   // Field로 만들지 않은 이유: 요청할 때마다 반환
+        return (int)(Math.random()*highest + 1);
+    }
+
+}
---
+++ src/Lec1013/SpeedGun.java
@@ -0,0 +1,24 @@
+package Lec1013;
+
+public class SpeedGun {
+
+    private int theSpeed;
+
+    public void  shot(Car car) {
+        System.out.println("—————speedgun monitor ———————");
+
+        int n = car.Navi.find();
+        for(int i=1 ; i <= n ; i++) {
+
+            theSpeed = car.current();
+            Road road = new Road();
+            int limit = road.getLimit();
+            System.out.println("Road" + i + "\t current speed:" + theSpeed + "km\t speed limit: :" + limit + "Km");
+            if (theSpeed >= limit) {
+                System.out.println("\t\t*\t\t ticked by" + (theSpeed - limit) + "Km over");
+            }
+
+        }
+
+    }
+}
