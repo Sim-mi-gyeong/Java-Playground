20aee9885248a220bc6da9b13bfe665925a003be
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2021-10-17 23:50:43 +0900

    0922 Conv2

---
+++ src/Lec0922/BitOperator.java
@@ -0,0 +1,26 @@
+package Lec0922;
+
+import java.util.Scanner;
+
+public class BitOperator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short a = 0;
+        while (true) {
+            int b = sc.nextInt();
+            if (b == 13) {
+                System.out.println("비트 설정 종료~");
+                break;
+            }
+            a = (short)(a | (1 << (b-1)));
+            System.out.printf("%03x\n",a);
+//            System.out.println("a: "+a);
+//            System.out.println("Integer.toHexString(a) : "+Integer.toHexString(a));
+//            System.out.println("Integer.toBinaryString(a) : "+Integer.toBinaryString(a));
+//            System.out.println("(byte)a : "+(byte)a);
+        }
+    }
+}
+
+
