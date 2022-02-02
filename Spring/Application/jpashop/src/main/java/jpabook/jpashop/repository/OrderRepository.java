13b7eccbbd590805744b2fe2457c710858ff2d4b
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-02-03 03:13:57 +0900

    OrderServiceTest & OrderSearch(JPA ver.)

--- Spring/Application/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ Spring/Application/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -19,11 +19,19 @@ public class OrderRepository {
     public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
+
     /**
      * 주문 내역 검색 로직 추후 구현
      */
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//        return;
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                        " where o.status = :status " +
+                        " and m.name like:name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+//                .setFirstResult(100)   // 100부터 시작해서 1000개를 가져옴(페이징 관련)
+//                .setMaxResults(1000)   // 최대 1000 개까지 조회
+                .getResultList();
+    }
 
 }
