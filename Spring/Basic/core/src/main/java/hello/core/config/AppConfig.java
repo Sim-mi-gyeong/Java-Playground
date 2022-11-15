6045e76e2f2e66d390626115a7b49e2a2b39fbd8
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-11-16 02:46:03 +0900

    [Feat] 할인 정책 변경에 따른  AppConfig 변경

--- Spring/Basic/core/src/main/java/hello/core/config/AppConfig.java
+++ Spring/Basic/core/src/main/java/hello/core/config/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core.config;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,11 +12,20 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
-    public MemberService memberService() {   // memoryMemberRepository 사용
-        return new MemberServiceImpl(new MemoryMemberRepository());   // 생성자를 통해 MemoryMemberRepository 할당
+    public MemberService memberService() {   // MemberService 의 구현은 -> MemberServiceImpl 사용
+        return new MemberServiceImpl(memberRepository());   // memberServiceImpl 애플리케이션에서 MemberRepository 에 대한 것은 -> MemoryMemberRepository 사용
+    }
+
+    private MemberRepository memberRepository() {   // 메드명을 통해 역할이 명시됨
+        return new MemoryMemberRepository();   // 이후 변경 사항 발생 시 해당 코드에서 구현 클래스만 변경
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
