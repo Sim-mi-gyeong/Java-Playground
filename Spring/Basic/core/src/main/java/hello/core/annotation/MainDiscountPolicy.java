9db8cb50ef196db6a4ac4880c7b3023366f2efd9
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2022-11-25 22:15:19 +0900

    [Feat] 의존관계 자동 주입

---
+++ Spring/Basic/core/src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,13 @@
+package hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {   // 이 애노테이션을 쓰면 해당 기능이 스프링 컨테이너에서 동작
+}
